ba85cc06a985de7fb748409b9cf5dcbe375a401e
Tolobekova Aiganysh <tolobekova01@gmail.com>
2023-03-09 18:56:44 +0600

    commit

--- src/main/java/com/example/homework49/service/CandidateDataModel.java
+++ src/main/java/com/example/homework49/service/CandidateDataModel.java
@@ -1,18 +1,19 @@
 package com.example.homework49.service;
 
 import com.example.homework49.model.Candidate;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 public class CandidateDataModel {
-    private List<Candidate> candidates;
-    private int allVotes;
+    private static List<Candidate> candidates;
+    private static int allVotes = 5;
 
-    public List<Candidate> getCandidates() {
+    public static List<Candidate> getCandidates() {
         return candidates;
     }
 
-    public void setCandidates(List<Candidate> candidates) {
+    public static void setCandidates(List<Candidate> candidates) {
         this.candidates = candidates;
     }
 
@@ -20,7 +21,7 @@ public class CandidateDataModel {
         this.candidates = candidates;
     }
 
-    public void generateAllVotes(){
+    public static void generateAllVotes(){
         for (Candidate candidate : candidates) {
             if (candidates.get(1).getVotes() != 0) {
                 allVotes = allVotes + candidate.getVotes();
@@ -28,16 +29,26 @@ public class CandidateDataModel {
         }
     }
 
-    public void setAllVotes(int allVotes) {
+    public static void setAllVotes(int allVotes) {
         this.allVotes = allVotes;
     }
 
-    public int getAllVotes() {
+    public static int getAllVotes() {
         generateAllVotes();
         return allVotes;
     }
 
-    public void checkCandidate(){
-        
+
+    public static Candidate checkCandidate(String candidateName){
+        Candidate candidate = null;
+        for (int i = 0; i < candidates.size(); i++) {
+            if (candidates.get(i).getName().equals(candidateName)){
+                candidate = candidates.get(i);
+                allVotes++;
+            }
+        }
+        candidate.setPercentVotes((candidate.getVotes()*100)/allVotes);
+        return candidate;
     }
+
 }
